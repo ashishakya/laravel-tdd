a094c79744400a147f0e757018d0ee21d3b19529
Ashish Shakya <ashishakya@outlook.com>
2021-11-01 22:35:03 +0545

    TDD-4 First green test
    - [x] Move withoutExceptionHandling() method to parent class
    - [x] Create controllers and methods

--- tests/Feature/TodoListTest.php
+++ tests/Feature/TodoListTest.php
@@ -16,12 +16,10 @@ class TodoListTest extends TestCase
     public function test_example()
     {
         //preparation (prepare)
-        $this->withoutExceptionHandling();
 
         //action (perform)
         $response = $this->getJson(route("api.todo-list.index")); // adds content type as json
 
-        dd($response->json());
         //assertion (predict)$res
         $this->assertEquals(1, count($response->json()));
     }
