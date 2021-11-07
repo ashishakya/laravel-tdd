8de63ecf15e5724aae5fac2b950cc09560663a0e
Ashish Shakya <ashishakya@outlook.com>
2021-11-07 21:49:59 +0545

    TDD-21 On delete cascade

--- database/migrations/2021_11_07_081128_create_tasks_table.php
+++ database/migrations/2021_11_07_081128_create_tasks_table.php
@@ -17,7 +17,8 @@ class CreateTasksTable extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->string("title");
-            $table->unsignedBigInteger("todo_list_id");
+//            $table->unsignedBigInteger();
+            $table->foreignId("todo_list_id")->constrained("todo_lists")->onDelete("cascade");
             $table->string("status")->default(Task::NOT_STARTED);
             $table->timestamps();
         });
--- tests/Unit/TodoListTest.php
+++ tests/Unit/TodoListTest.php
@@ -9,9 +9,13 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Collection;
 use Tests\TestCase;
 
+/**
+ *
+ */
 class TodoListTest extends TestCase
 {
     use RefreshDatabase;
+
     /**
      * @test
      */
@@ -22,5 +26,21 @@ class TodoListTest extends TestCase
 
         $this->assertInstanceOf(Collection::class, $todoList->tasks);
         $this->assertInstanceOf(Task::class, $todoList->tasks->first());
+    }
+
+    /**
+     * @test
+     */
+    public function if_todo_list_is_deleted_all_its_tasks_are_deleted()
+    {
+        $todoList = TodoList::factory()->create();
+        $task     = Task::factory()->create(["todo_list_id" => $todoList->id]);
+        $anotherTask = Task::factory()->create();
+
+        $todoList->delete();
+
+        $this->assertDatabaseMissing("todo_lists", ["id" => $todoList->id]);
+        $this->assertDatabaseMissing("tasks", ["id" => $task->id]);
+        $this->assertDatabaseHas("tasks", ["id" => $anotherTask->id]);
     }
 }
