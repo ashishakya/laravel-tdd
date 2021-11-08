d06106aed35dd7872c6c2cd1eddafbb16d0b23a5
Ashish Shakya <ashishakya@outlook.com>
2021-11-08 07:19:08 +0545

    TDD-23.2 User login with valid user name and password

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,15 +4,16 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Requests\Auth\LoginRequest;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
 
 class LoginController
 {
     public function __invoke(LoginRequest $loginRequest)
     {
-        $user = User::where("email", $loginRequest->email);
+        $user = User::where("email", $loginRequest->email)->first();
 
-        if ( !$user ) {
+        if ( !$user || !Hash::check($loginRequest->password, $user->password)) {
             return response("Credential do not match", Response::HTTP_UNAUTHORIZED);
         }
 
--- tests/Feature/Auth/LoginTest.php
+++ tests/Feature/Auth/LoginTest.php
@@ -2,20 +2,53 @@
 
 namespace Tests\Feature\Auth;
 
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
+/**
+ *
+ */
 class LoginTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * @test
      */
     public function a_user_can_login_with_email_and_password()
     {
+        $user = User::factory()->create();
         $response = $this->postJson(route("api.login"), [
-            "email"    => "ashish@ashish.com",
-            "password" => "secreta",
+            "email"    => $user->email,
+            "password" => "password", // defined in factory
         ])->assertOk();
 
         $this->arrayHasKey("token", $response->json());
+    }
+
+    /**
+     * @test
+     */
+    public function email_and_password_is_required_for_login()
+    {
+        $response = $this->postJson(route("api.login"), [
+            "email"    => "ashish@ashish.com",
+            "password" => "secret",
+        ])->assertUnauthorized();
+    }
+
+    /**
+     * @test
+     */
+    public function raise_error_if_password_is_incorrect()
+    {
+        $user = User::factory()->create();
+
+        $this->postJson(route("api.login"), [
+            "email"    => $user->email,
+            "password" => "randomPassword",
+        ])->assertUnauthorized();
+
     }
 }
